2172ef032bee37fe30ffab4834cfb26b7edbc59e
proPianist1124 <phantomforces11242009@gmail.com>
2024-09-01 17:33:51 -0700

    updated redirect url

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,9 @@
 <script>
-    import { PUBLIC_GITHUB_CLIENT_ID } from "$env/static/public";
+    import { PUBLIC_GITHUB_CLIENT_ID, PUBLIC_URL } from "$env/static/public";
 
-    let redirect = "http://localhost:5173/api/github";
     let path = "/";
 </script>
 
-<a href="https://github.com/login/oauth/authorize?client_id={PUBLIC_GITHUB_CLIENT_ID}&redirect_uri={redirect}&scope=user:email" style="text-decoration: none;">
+<a href="https://github.com/login/oauth/authorize?client_id={PUBLIC_GITHUB_CLIENT_ID}&redirect_uri={PUBLIC_URL}&scope=user:email" style="text-decoration: none;">
     <button style="display: block; margin: auto; margin-top: 100px; cursor: pointer;">Login with Github</button>
 </a>
